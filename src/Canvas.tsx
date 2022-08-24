743477f4c5a65e9526bea21761aafff449b914c3
Tsutomu Yamaguchi <t.yamaguchi.air@outlook.jp>
2022-08-25 03:38:58 +0900

    シミュレーションの初期化と更新部を追加

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,20 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import p5 from "p5";
 import './Canvas.css';
+import { Cell } from './cell'
 import { clear } from "console";
+import { createTextChangeRange } from "typescript";
 
 const width = 500;
 const height = 500;
-const cell_length = 20;
-
-const cells: any = [];
-
-let dt = 0.1;
-let dx = 0.6;
-let Du = 0.02 / (dx*dx);
-let Dv = 0.5 / (dx*dx);
-let f = 0.022;
-let k = 0.051;
+const cell_size = 1;
+const cell_length = 100;
 
 const SQUARE_SIZE = 50;
 const square_start = width / 2 - SQUARE_SIZE /2;
@@ -23,80 +17,124 @@ const square_end = width / 2 + SQUARE_SIZE /2;
 const uMax = 7.5;
 const vMax = 10.0;
 
-class Cell{
-    neighbor: any[];
-    u: number;
-    v: number;
-    u_reacted: number;
-    v_reacted: number;
-    u_spreaded: number;
-    v_spreaded: number;
+const STEP = 5000;
 
-    constructor(){
-        this.neighbor = [];
-        this.u = 0.0;
-        this.v = 0.0;
-        this.u_reacted = 0.0;
-        this.v_reacted = 0.0;
-        this.u_spreaded = 0.0;
-        this.v_spreaded = 0.0;
+const cells = Array(width).fill(null).map(row => new Array(height).fill(null))
+
+const initialization = () =>{
+    
+    for(let x=0; x<width; x++){
+        for(let y=0; y<height; y++){
+            let cell = new Cell();
+            cells[x][y] = cell;
+        }
     }
-
-    spreaded(){
-        let sum_u = 0.0;
-        let sum_v = 0.0;
-
-        this.neighbor.map((nei) =>{
-            sum_u += nei.u;
-            sum_v += nei.v;
-        });
-        this.u_spreaded = Du * (sum_u - 4.0 * this.u);
-        this.v_spreaded = Dv * (sum_v - 4.0 * this.v);
+    for(let x=0; x<width; x++){
+        for(let y=0; y<height; y++){
+            cells[x][y].neighbor.push(cells[x][Math.max(0, y-1)])
+            cells[x][y].neighbor.push(cells[Math.min(x+1, width-1)][y])
+            cells[x][y].neighbor.push(cells[x][Math.min(height-1, y+1)])
+            cells[x][y].neighbor.push(cells[Math.max(x-1, 0)][y])
+        }
     }
-
-    reacted(){
-        this.u_reacted = this.u * this.u * this.v - (f+k) * this.u;
-        this.u_reacted = Math.max(0, Math.min(this.u_reacted, uMax));
-        this.v_reacted = -1 * this.u * this.u * this.v + f * (1-k);
-        this.v_reacted = Math.max(0, Math.min(this.v_reacted, vMax));
+    for(let x=square_start; x<(SQUARE_SIZE+square_start); x++){
+        for(let y=square_start; y<(SQUARE_SIZE+square_start); y++){
+            cells[x][y].u = 0.5;
+            cells[x][y].v = 0.25;
+            console.log("x: " + x + ", y: " + y + ", u: " + cells[x][y].u)
+        }
     }
-
-    result(){
-        this.u = this.u + dt * (this.u_spreaded + this.u_reacted);
-        this.v = this.v + dt * (this.v_spreaded + this.v_reacted);
-    }
+        
 
 }
-//描画
-const draw = () =>{
+
+const update = () =>{
+    const dt = 0.1
+    for(let x=0; x<width; x++){
+        for(let y=0; y<height; y++){
+            cells[x][y].reacted();
+            cells[x][y].spreaded();
+        }
+    }
+    for(let x=0; x<width; x++){
+        for(let y=0; y<height; y++){
+            let c = cells[x][y];
+            //cells[x][y].sum_result();
+            cells[x][y].u = c.u + dt * (c.u_spreaded + c.u_reacted);
+            cells[x][y].v = c.v + dt * (c.v_spreaded + c.v_reacted);
+
+            console.log("x: " + x + ", y: " + y + ", u: " + cells[x][y].u)
+    }
+}
 
 }
+
+const style = {
+    border: '1px solid black',
+    backgroundColor: 'white',
+  };
 
 const Canvas = () => {
     const canvasRef = useRef(null);
-
+    const [loaded, setLoaded] = useState(false);
     const getContext = (): CanvasRenderingContext2D => {
         const canvas: any = canvasRef.current;
 
         return canvas.getContext('2d');
     };
-
-    useEffect(() => {
+    
+    //初期化処理
+    useEffect(()=>{
         const ctx: CanvasRenderingContext2D = getContext();
-        ctx.fillRect(0,0, width, height);
-        ctx.save();
-
-        ctx.fillStyle = 'rgb(255, 165, 0)';
-        ctx.fillRect(square_start, square_start, SQUARE_SIZE, SQUARE_SIZE);
-        ctx.save();
-    }, [])
-
+        initialization();
+        for(let x=0; x<width; x++){
+            for(let y=0; y<height; y++){
+                let c = cells[x][y];
+                let r = (c.u/uMax)*255;
+                let b = (c.v/vMax)*255;
+                ctx.fillStyle='rgb(' + r + ',' + r + ',' + b + ')';
+                ctx.rect(x,y,1,1);
+            }
+        }
+        setLoaded(true);
+        ctx.fill();
+    });
+    
+    let count = 0;
+    useEffect(()=>{
+        const ctx: CanvasRenderingContext2D = getContext();
+        while(loaded){
+            update();
+            for(let x=0; x<width; x++){
+                for(let y=0; y<height; y++){
+                    let c = cells[x][y];
+                    let r = (c.u/uMax)*255;
+                    let b = (c.v/vMax)*255;
+                    ctx.fillStyle='rgb(' + r + ',' + r + ',' + b + ')';
+                    ctx.rect(x,y,1,1);
+                }
+            }
+            ctx.fill();
+            count++;
+            if(count>STEP){
+                setLoaded(false);
+            }
+        }
+    });
+    
     return(
         <div>
-            <canvas width={width} height={height} id="canvas" className=".canvas-border" ref={canvasRef}></canvas>
+            <canvas
+                width={width} 
+                height={height} 
+                ref={canvasRef}
+                id="canvas" 
+                style={style}
+                />
         </div>
         
     );
 }
+
 
 export default Canvas;
